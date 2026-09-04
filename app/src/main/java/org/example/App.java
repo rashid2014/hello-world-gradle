56052a0314ae499924c5b3ec1cfb4f36e177cb1c
Harun Ur Rashid <harunnec@gmail.com>
2026-09-04 19:35:35 -0400

    Initial Build

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Hello Metro College!";
+        return "Hello Pragra!";
     }
 
     public static void main(String[] args) {
